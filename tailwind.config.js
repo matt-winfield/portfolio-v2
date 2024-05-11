aafc1d339679854598e9c436fabe5811119b2ad8
Matthew Winfield <matthew.thomas.winfield@gmail.com>
2024-05-11 11:33:17 +0100

    Add prettier

---
+++ tailwind.config.js
@@ -0,0 +1,9 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+    content: ['./app/**/*.{ts,tsx,jsx,js}'],
+    darkMode: 'class',
+    theme: {
+        extend: {},
+    },
+    plugins: [],
+};
