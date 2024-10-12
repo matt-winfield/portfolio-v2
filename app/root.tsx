30483b16cba5dacaef79dedcf24e5901ca3c923c
Matthew Winfield <matthew.thomas.winfield@gmail.com>
2024-10-12 13:14:50 +0100

    Update favicon

--- app/root.tsx
+++ app/root.tsx
@@ -94,6 +94,49 @@ function Layout({ children }: { children: React.ReactNode }) {
                     data-domain="matt-winfield.com"
                     src="https://pl.matt-winfield.com/js/script.js"
                 ></script>
+                {/* Standard favicon */}
+                <link rel="icon" type="image/x-icon" href="favicon.ico" />
+
+                {/* Specific size images */}
+                <link
+                    rel="icon"
+                    type="image/png"
+                    sizes="16x16"
+                    href="favicon-16.png"
+                />
+                <link
+                    rel="icon"
+                    type="image/png"
+                    sizes="32x32"
+                    href="favicon-32.png"
+                />
+                <link
+                    rel="icon"
+                    type="image/png"
+                    sizes="64x64"
+                    href="favicon-64.png"
+                />
+
+                {/* Apple touch icons */}
+                <link
+                    rel="apple-touch-icon"
+                    sizes="180x180"
+                    href="favicon-180.png"
+                />
+
+                {/* Android icons */}
+                <link
+                    rel="icon"
+                    type="image/png"
+                    sizes="192x192"
+                    href="favicon-192.png"
+                />
+                <link
+                    rel="icon"
+                    type="image/png"
+                    sizes="512x512"
+                    href="favicon-512.png"
+                />
                 <Meta />
                 <Links />
             </head>
